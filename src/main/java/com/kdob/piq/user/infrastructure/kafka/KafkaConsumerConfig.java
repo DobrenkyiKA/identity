d6568aa48c02f1afe429832f32561e69902e3ad0
Konstantin Dobrenkii <6092822@gmail.com>
2026-04-19 23:41:53 +0400

    Update Kafka configuration and version across services
    Added support for Kafka topic creation in user service and fixed property key issues in the auth server. Also upgraded Kafka Docker image from 4.0.0 to 4.2.0 for improved compatibility and stability.

--- src/main/java/com/kdob/piq/user/infrastructure/kafka/KafkaConsumerConfig.java
+++ src/main/java/com/kdob/piq/user/infrastructure/kafka/KafkaConsumerConfig.java
@@ -1,5 +1,7 @@
 package com.kdob.piq.user.infrastructure.kafka;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
